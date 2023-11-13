7b72bf0452323de5059fd4c3772a1f07334d9284
ISSuh <yoyus5@naver.com>
2023-11-13 16:40:39 +0900

    add test

---
+++ transcoding/src/main/java/mystream/transcoding/TranscodingApplication.java
@@ -0,0 +1,13 @@
+package mystream.transcoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranscodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranscodingApplication.class, args);
+	}
+
+}
---
+++ transcoding/src/test/java/mystream/transcoding/TranscodingApplicationTests.java
@@ -0,0 +1,13 @@
+package mystream.transcoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranscodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
